44118933ac8bdf59f0a42ada006a018fa4f81214
aksaakov <a.cealicov@gmail.com>
2025-08-19 00:58:37 +0100

    added battery monitoring

--- esp32_lora_remote.ino
+++ esp32_lora_remote.ino
@@ -1,14 +1,18 @@
 #include "DisplaySuite.h"
 #include "loraController.h"
 #include "LoRaWan_APP.h"
+#include "BatteryMonitoring.h"
 
 bool isAlarmArmed = false; 
 bool isAlarmOn = false;
 bool isMotionTriggered = false;
 
+int VOLTAGE_PIN = 2; // Pin connected to the voltage sensor output
 int PIR_PIN = 48;
 int ALARM_LED_PIN = 45;
+
 uint8_t motion_pkg = 0x01;
+volatile uint8_t pendingAck = 0;
 
 void sendMessage(const uint8_t* package) {
   Radio.Standby();
@@ -18,6 +22,7 @@ void sendMessage(const uint8_t* package) {
 
 void setup() {
   Serial.begin(115200);
+  analogReadResolution(12); // ESP32 default is 12 bits
   delay(200);
   displayLogo();
   pinMode(PIR_PIN, INPUT);
@@ -29,8 +34,13 @@ void processReceivedPacket(const uint8_t* data, uint16_t len, int16_t rssi, int8
   Serial.printf("data=%u, rssi=%d, snr=%d\n", data[0], rssi, snr);
 
   switch(data[0]) {
+    case 0x01:
+      Serial.println("echoed -> 0x01 code: TX failed?");
+      radioInit(); //reset radio in case of echo
+      break;
     case 0x10:
       Serial.println("received -> 0x10 code: ALARM ARMED");
+      pendingAck = 0x10;
       digitalWrite(ALARM_LED_PIN, HIGH);
       isAlarmArmed = true;
       if (isMotionTriggered) {
@@ -39,6 +49,7 @@ void processReceivedPacket(const uint8_t* data, uint16_t len, int16_t rssi, int8
       break;
     case 0x11:
       Serial.println("received -> 0x11 code: ALARM DIDSARMED");
+      pendingAck = 0x11;
       digitalWrite(ALARM_LED_PIN, LOW);
       isAlarmArmed = false;
       isAlarmOn = false;
@@ -51,12 +62,27 @@ void processReceivedPacket(const uint8_t* data, uint16_t len, int16_t rssi, int8
 }
 
 void loop() {
+  float voltage = readBatteryVoltage();
+  int percent = batteryPercent(voltage);
+
+  Serial.print("Battery Voltage: ");
+  Serial.print(voltage, 2);
+  Serial.print(" V  |  Battery: ");
+  Serial.print(percent);
+  Serial.println("%");
+
   int motionState = digitalRead(PIR_PIN);
+
+  if (pendingAck) {
+    uint8_t ackPackage = pendingAck;
+    pendingAck = 0;
+    sendMessage(&ackPackage);
+  }
 
   if (motionState == HIGH) {
     displayMotionIcon();
     sendMessage(&motion_pkg);
-    isMotionTriggered = true;
+    if (isAlarmArmed) isMotionTriggered = true;
   }
 
   if (isAlarmOn) {
@@ -65,6 +91,6 @@ void loop() {
 
   Radio.IrqProcess();
 
-  delay(1000);
+  if (!pendingAck) delay(1000);
   // receive();
 }
